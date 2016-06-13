d1f278f23c5b47083452a27dc8f37d7acb8571f8
atungare <ajay.tungare@gmail.com>
2016-06-13 10:29:12 -0700

    e39

--- haskell/e39.hs
+++ haskell/e39.hs
@@ -1,7 +1,7 @@
 import Data.List
 
 tripleFinder :: Int -> [[Int]]
-tripleFinder n = [[a,b,c] | a <- [1..n], b <- [1..n], c <- [n-a-b], a*a + b*b == c*c]
+tripleFinder n = [[a,b,c] |  l <- [div n 2], a <- [1..l], b <- [1..l], c <- [n-a-b], a*a + b*b == c*c, b >= a]
 
-main = do putStrLn $ show  $ map tripleFinder [1..1000]
+main = do putStrLn $ show $ maximumBy (\(_,a) (_,b) -> compare a b) $ map (\x -> (x, (length . tripleFinder) x)) [1..1000]
 
